b9374a08e04a277e8d3d45bc78bd575a18fedd77
Salakhov Artur <gsmilexd@gmail.com>
2015-05-15 17:03:00 +0300

    Different changes(look at description)
    1) Test Master Server
    2) meta files for levels
    3) added new levels

---
+++ Tower Defence/Assets/Test Master Server/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	private bool isFirst;
+	private string ipAddress = "52.17.244.15";
+	// Use this for initialization
+	void Start () {
+		isFirst = true;
+		// Master Server
+		MasterServer.ipAddress = ipAddress;
+		MasterServer.port = 23466;
+
+		// Facilitator
+		Network.natFacilitatorIP = ipAddress;
+		Network.natFacilitatorPort = 50005;
+
+
+	
+	}
+
+	void OnGUI()
+	{
+		if (isFirst) {
+			Debug.Log ("hi");
+			Network.InitializeServer (2, 25000, true);
+		
+			MasterServer.RegisterHost ("MyRoom", "Name", "ddd");
+			isFirst =false;
+		}
+	}
+
+	void OnMasterServerEvent()
+	{
+		Debug.Log ("register");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
